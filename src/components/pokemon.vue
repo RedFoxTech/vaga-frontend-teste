295b0f25739de961a1209dab8499108f0e3e5455
cicinth <cinthiaqcardoso@hotmail.com>
2018-06-10 12:54:22 -0300

    filtro de pokemon

--- src/components/pokemon.vue
+++ src/components/pokemon.vue
@@ -1,17 +1,18 @@
 <template>
-  <div>
+  <div class ="container    ">
     <h1>{{titulo}}</h1>
-    
-     <ul class="list-group">
-      <li class="list-group-item" v-for="pokemon in pokemons.results" v-text="pokemon.name"></li>
+    <input type ="search"  class ="filtro" v-on:input="filtro=$event.target.value" placeholder="pesquisa por parte nome do pokemon">
+
+    <ul class="list-group">
+      <li class="list-group-item" v-for="pokemon of pokemonComFiltro" v-text="pokemon.name"></li>
         <!-- <li v-for="pokemon in pokemons.results">
             <img :src="pokemon.sprites" alt="lala" height="42" width="42">
           </li>
           -->
     </ul>
-      <button class="btn btn-primary" v-if="pokemons.previous" @click="previous">Previous</button>
-       <button class="btn btn-primary" v-if="pokemons.next" @click="next">Next</button>
-    
+
+     <button class="btn btn-primary" v-if="pokemons.previous" @click="previous">Previous</button>
+      <button class="btn btn-primary" v-if="pokemons.next" @click="next">Next</button>
   </div>
   
 </template>
@@ -24,15 +25,34 @@ export default {
     return{
       titulo:'pokedexx',
       pokemons:{},
+      //imgPoke:[],
+      filtro:'',
     }
   },
- 
- created(){
-    this.fetchPokemons();
-    
-   
+  computed:{
+        pokemonComFiltro(){
+          if(this.filtro){
+              let exp = new RegExp(this.filtro.trim(), 'i')
+              return this.pokemons.results.filter(pokemon => exp.test(pokemon.name))
+            } else{
+                return this.pokemons.results
+             }  
+        }       
   },
-  methods: {
+  
+
+ created(){
+    this.fetchPokemons();    
+    //this.imgPoke();
+  },
+  // computed:{
+    //    imagemPokemon:function(){
+      //      for(po in pokemons) {
+        //            return this.po.split('').reverse().join('')
+          //      }
+        //}
+   // },
+     methods: {
       fetchPokemons(url = 'http://pokeapi.co/api/v2/pokemon') {
         axios.get(url)
           .then(({ data }) => this.pokemons = data); [
@@ -49,10 +69,27 @@ export default {
       },
       previous(){
         this.fetchPokemons(this.pokemons.previous);
-      }
+      },
+      /*imgPoke(url = 'http://pokeapi.co/api/v2/pokemon/1'){
+        axios.get(url)
+         // .then(({ data }) => this.imgPoke = data); [
+          //]
+     .then(function (data){
+        console.log(data);
+         })
+          .catch(function (error) {
+               console.log(error);
+           });
+          }*/
+
     }   
+
+
   } 
 </script>
 
 <style lang="scss">
+.filtro{
+    width: 100%;
+}
 </style>
