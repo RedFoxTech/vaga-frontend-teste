ecd25531d2e4a4a4f7f42e089f88c38494230e85
Victor Hugo <victorshgo@outlook.com>
2019-02-18 11:45:03 -0300

    Desafio - RedFox Tech

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Pokemon/>
+  </div>
+</template>
+
+<script>
+import Pokemon from './components/Pokemon'
+
+export default {
+  name: 'App',
+  components: {
+    Pokemon
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
