25598c8750b9a9b0b1a5a1cbdbf265a6e6c4c9bc
dannpl <dancardosobr@gmail.com>
2019-03-21 18:29:14 -0300

    Heroku App

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
+ 
 const app = express();
-
-app.use(express.static(__dirname + './dist'));
-
-app.listen(process.env.PORT || 8080);
-
+ 
+// Serve only the static files form the angularapp directory
+app.use(express.static(__dirname + '/pokemon'));
+ 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+ 
+res.sendFile(path.join(__dirname+'/pokemon/index.html'));
 });
-
+ 
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
