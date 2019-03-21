754fb2c54a1e150ad21721b1d6f0f9a47a9c55a1
dannpl <dancardosobr@gmail.com>
2019-03-21 18:10:53 -0300

    da

--- server.js
+++ server.js
@@ -4,14 +4,11 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-// Replace the '/dist/<to_your_project_name>'
-app.use(express.static('./dist/pokemon'));
+app.use(express.static(__dirname + './dist'));
+
+app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req,res) {
-  // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/pokemon/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
 
