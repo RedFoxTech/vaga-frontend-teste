405318d778b6f1512251cb87c718790408f06a6a
dannpl <dancardosobr@gmail.com>
2019-03-21 16:28:20 -0300

    make it better

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+//Install express server
 const express = require('express');
-const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname+'/dist'));
+const app = express();
 
-app.listen(process.env.PORT||8080);
+// Serve only the static files form the dist directory
+// Replace the '/dist/<to_your_project_name>'
+app.use(express.static('./dist/pokemon'));
 
-
-//Path Location Strategy
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+app.get('/*', function(req,res) {
+  // Replace the '/dist/<to_your_project_name>/index.html'
+  res.sendFile(path.join(__dirname + '/dist/pokemon/index.html'));
 });
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
+
