c773310a58a98c5d01e47f06b0fcbe2ffbf79e25
Kelly Lima <keely06@live.com>
2019-05-30 19:40:16 -0300

    evento botão

--- js/script.js
+++ js/script.js
@@ -1,39 +1,42 @@
 const input = document.querySelector('input');
 const btn = document.querySelector('button');
-const container = document.querySelector('.container')
+const container = document.querySelector('.container');
 
-fetch('https://pokeapi.co/api/v2/pokemon/')
-.then((response) => {
-    return response.json()
-})
-.then((data) => {
-    data.results.forEach((item) => {
 
-        let card = document.createElement('div');
-        card.setAttribute('class', 'card');
-        container.appendChild(card);
-
-        let p = document.createElement('p');
-        p.innerHTML = item.name;
-        card.appendChild(p);
-
-        let url = item.url;
-        fetch(`${url}`)
+btn.addEventListener('click', () => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${input.value}`)
         .then((response) => {
-            return response.json();
+            return response.json()
         })
         .then((data) => {
-            const arr = [data];
-            // console.log(arr);
-            arr.forEach((item) => {
-                // console.log(item.sprites.back_default);
-                let img = document.createElement('img');
-                img.setAttribute('src', item.sprites.front_default)
-                card.appendChild(img);
+            data.results.forEach((item) => {
+
+                let card = document.createElement('div');
+                card.setAttribute('class', 'card');
+                container.appendChild(card);
+
+                let p = document.createElement('p');
+                p.innerHTML = item.name;
+                card.appendChild(p);
+
+                let url = item.url;
+                fetch(`${url}`)
+                    .then((response) => {
+                        return response.json();
+                    })
+                    .then((data) => {
+                        const arr = [data];
+                        // console.log(arr);
+                        arr.forEach((item) => {
+                            // console.log(item.sprites.back_default);
+                            let img = document.createElement('img');
+                            img.setAttribute('src', item.sprites.front_default)
+                            card.appendChild(img);
+                        })
+                    })
             })
         })
-    })
-})
-.catch((erro) => {
-    console.log(erro)
+        .catch((erro) => {
+            console.log(erro)
+        })
 })
