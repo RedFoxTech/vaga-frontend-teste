8beb1d9aa0b7ba1f5b38784306da9e2ccf9d913d
Kelly Christina <keely06@live.com>
2019-05-31 13:44:43 -0300

    funçao modal

--- js/script.js
+++ js/script.js
@@ -1,39 +1,60 @@
-const input = document.querySelector('input');
-const btn = document.querySelector('button');
-const container = document.querySelector('.container')
+const root = document.querySelector('#root')
+const myContainerModal = document.querySelector('[data-modal="container"]');
+const btnClose = document.querySelector('[data-modal="close"]');
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container-fluid');
+root.appendChild(container);
+
+const row = document.createElement('div');
+row.classList.add('row', 'd-flex', 'justify-content-center')
+container.appendChild(row);
+
 
 fetch('https://pokeapi.co/api/v2/pokemon/')
-.then((response) => {
-    return response.json()
-})
-.then((data) => {
-    data.results.forEach((item) => {
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        data.results.forEach((item) => {
 
-        let card = document.createElement('div');
-        card.setAttribute('class', 'card');
-        container.appendChild(card);
+            const card = document.createElement('div');
+            card.classList.add('m-5', 'p-5', 'bg-danger', 'border', 'border-dark', 'rounded-lg', 'shadow-sm')
+            row.appendChild(card);
 
-        let p = document.createElement('p');
-        p.innerHTML = item.name;
-        card.appendChild(p);
+            const p = document.createElement('p');
+            p.innerHTML = item.name.toUpperCase();
+            p.classList.add('text-white', 'text-center');
+            card.appendChild(p);
 
-        let url = item.url;
-        fetch(`${url}`)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            const arr = [data];
-            // console.log(arr);
-            arr.forEach((item) => {
-                // console.log(item.sprites.back_default);
-                let img = document.createElement('img');
-                img.setAttribute('src', item.sprites.front_default)
-                card.appendChild(img);
+            const btnCard = document.createElement('button');
+            btnCard.innerHTML = 'Saiba mais';
+            btnCard.classList.add('d-block', 'btn', 'btn-outline-light')
+
+            btnCard.addEventListener('click', () => {
+                myContainerModal.classList.add('ativo');
             })
+
+            btnClose.addEventListener('click', () => {
+                myContainerModal.classList.remove('ativo');
+            })
+
+            const url = item.url;
+            fetch(`${url}`)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+                    const arr = [data];
+                    arr.forEach((item) => {
+                        const img = document.createElement('img');
+                        img.setAttribute('src', item.sprites.front_default);
+                        card.appendChild(img);
+                        card.appendChild(btnCard);
+                    })
+                })
         })
     })
-})
-.catch((erro) => {
-    console.log(erro)
-})
+    .catch((erro) => {
+        console.log(erro)
+    })
