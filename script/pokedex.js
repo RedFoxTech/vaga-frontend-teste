c4f60e2b1ddaa72ac5934032b9e345a2bc6560a8
Erika <erika.sayuri17@gmail.com>
2019-06-01 23:30:37 -0300

    Criando as cards e trazendo o nome e a imagem do pokemon

--- script/pokedex.js
+++ script/pokedex.js
@@ -6,7 +6,34 @@ fetch('https://pokeapi.co/api/v2/pokemon/')
 })
 .then((data)=>{
     console.log(data)
+    data.results.forEach(pokemon => {  
+        const card = document.createElement("div");
+        card.setAttribute("class", "card");
+        container.appendChild(card);
     
+        const nomePokemon = document.createElement("h1");
+        nomePokemon.innerHTML = pokemon.name;
+        card.appendChild(nomePokemon)
+
+        const url = pokemon.url;
+        fetch(url)
+        .then((response)=>{
+            return response.json()
+        })
+        .then((data)=>{
+            console.log(data)
+            const informacao = [data]
+            informacao.forEach(cada => {
+                const imagem = document.createElement("img");
+                imagem.setAttribute("src", cada.sprites.front_default)
+                card.appendChild(imagem);
+            });
+
+        })
+        .catch((erro=>{
+            console.log("erro da url")
+        }))
+    });
 })
 .catch((erro)=>{
     console.log("erro")
