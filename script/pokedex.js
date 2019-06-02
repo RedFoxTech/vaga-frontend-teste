012d38441f915eafba2c92a314142c1110ac8e10
Erika <erika.sayuri17@gmail.com>
2019-06-01 22:23:59 -0300

    Iniciando página pokedex

--- script/pokedex.js
+++ script/pokedex.js
@@ -1,2 +1,13 @@
 const container = document.querySelector(".container");
 
+fetch('https://pokeapi.co/api/v2/pokemon/')
+.then((response)=>{
+    return response.json()
+})
+.then((data)=>{
+    console.log(data)
+    
+})
+.catch((erro)=>{
+    console.log("erro")
+})
