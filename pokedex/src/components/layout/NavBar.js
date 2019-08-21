bd51bb2ec279fd28138eb56b730cf3fc31a6ab75
Dioginis <dioginiscarvalho@hotmail.com>
2019-08-21 13:50:44 -0300

    feat: add search bar

--- pokedex/src/components/layout/NavBar.js
+++ pokedex/src/components/layout/NavBar.js
@@ -4,11 +4,24 @@ import styled from "styled-components";
 export default class extends Component {
   render() {
     return (
-      <div>
-        <nav className="navbar">
-          <a>Pokedex</a>
-        </nav>
-      </div>
+      <nav className="navbar navbar-expand-lg navbar-dark teal lighten-2 mb-4">
+        <a className="navbar-brand" href="http://localhost:3000/">
+          Pokedex
+        </a>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <form className="form-inline ml-auto">
+            <div className="md-form my-0">
+              <input
+                className="form-control mr-sm-2"
+                type="text"
+                placeholder="Enter pokemon name"
+                aria-label="Search"
+              />
+            </div>
+          </form>
+        </div>
+      </nav>
     );
   }
 }
