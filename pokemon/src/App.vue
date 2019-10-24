740ff0c081aaf7094b6ecbc82307fdfae6b389f9
Felipe <felipeabila@hotmail.com>
2019-10-24 00:53:06 -0300

    Initial Commit

---
+++ pokemon/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img id="logo" alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app'
+  // components: {
+  //   HelloWorld
+  // }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Lato', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#logo {
+  height: 120px;
+}
+</style>
