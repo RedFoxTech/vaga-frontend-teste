d4d72ba8cfd1be336f1b868d0ab15c910171e354
MauricioDantas <mauriciodantas.contato01@bol.com.br>
2019-12-18 03:33:11 -0300

    add component Paginate

---
+++ src/components/Paginate.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button
+      @click="getAllPokemons(paginate.prev)"
+      :disabled="paginate.prev == null"
+      class="btn btn-small">
+      Voltar
+    </button>
+    <button
+      @click="getAllPokemons(paginate.next)"
+      :disabled="paginate.next == null"
+      class="btn btn-small">
+      Avançar
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'paginate',
+  props: {
+    getAllPokemons: Function,
+    paginate: Object
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+button {
+  margin: 5px;
+}
+</style>
