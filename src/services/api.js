e902784a672a035304d476b436f7c0e0658ea3b7
MauricioDantas <mauriciodantas.contato01@bol.com.br>
2019-12-18 03:23:50 -0300

    add functions pokemonEspecifico e pokemonByType

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,17 @@
 import { http } from './config'
 
 export default {
-  pokemon: (page = 0) => {
+  pokemon: (page=0) => {
     return http.get(`/pokemon/?offset=${page}&limit=20`)
   },
   ability: (name) => {
     return http.get(`/ability/${name}`)
   },
-  pokemonStatus: (param) => {
+  pokemonEspecifico: (param) => {
     return http.get(`/pokemon/${param}`)
+  },
+  pokemonByType: (name) => {
+    return http.get(`/type/${name}`)
   },
   typePokemon: () => {
     return http.get('/type')
