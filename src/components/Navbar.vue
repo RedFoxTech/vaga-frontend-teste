76362b77a751bd18997d141b00aa8a24dc8e13b0
MauricioDantas <mauriciodantas.contato01@bol.com.br>
2019-12-18 18:33:59 -0300

    removendo ul's

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,29 +6,14 @@
           <img src="../assets/img/pokeball.png" width="50" height="50">
           Pokemon
         </a>
-        <a @click="sideNav()" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-        <ul class="right hide-on-med-and-down">
-          <li><a href="#">Sass</a></li>
-        </ul>
       </div>
     </nav>
-
-    <ul class="sidenav" id="mobile-demo">
-      <li><a href="#">Sass</a></li>
-    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'navbar',
-  methods: {
-    sideNav () {
-      var elems = document.querySelector('.sidenav');
-      var instances = M.Sidenav.init(elems);
-      instances.open()
-    }
-  }
+  name: 'navbar'
 }
 </script>
 
